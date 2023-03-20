7ac1e3626972fa21d5dea36d20c98e07e90c7874
Dylan Dao <dylanpdao@gmail.com>
2023-03-20 23:43:42 +0000

    added object logs + bracket notation

--- javascript-objects/main.js
+++ javascript-objects/main.js
@@ -9,16 +9,19 @@ student.livesInIrvine = false;
 student.previousOccupation = 'restauranteur';
 console.log('Value of student.livesInIrvine:', student.livesInIrvine);
 console.log('Value of student.previousOccupation:', student.previousOccupation);
+console.log('Value of student:', student);
 
 const vehicle = {
   make: 'Nissan',
   model: '240z',
   year: 1974
 };
-vehicle.color = 'tobacco metallic';
+vehicle['color'] = 'tobacco metallic';
 vehicle['isConvertible'] = false;
+console.log(vehicle);
 console.log('Value of vehicle.color:', vehicle['color']);
 console.log('Value of vehicle.isConvertible:', vehicle['isConvertible']);
+console.log('Value of vehicle:', vehicle);
 
 const pet = {
   name: 'Cinnamon',
@@ -27,3 +30,4 @@ const pet = {
 delete pet.name;
 delete pet.type;
 console.log('value of pet.name', pet.name);
+console.log('Value of pet:', pet);
