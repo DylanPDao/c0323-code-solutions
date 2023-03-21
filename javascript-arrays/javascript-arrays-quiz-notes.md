6f731fd7b7380d82e734001ff38de76ed474e2f1
Dylan Dao <dylanpdao@gmail.com>
2023-03-21 01:13:53 +0000

    finished

--- javascript-arrays/javascript-arrays-quiz-notes.md
+++ javascript-arrays/javascript-arrays-quiz-notes.md
@@ -5,17 +5,18 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - What are arrays used for?
-
+arrays are used to store data that is accessible by position
 - Describe array literal notation.
-
+where you define a new array using just empty brackets
 - How are arrays different from "plain" objects?
-
+objects represent 'things' with characteristics aka name:dylan
+arrays store lists of data as single variables
 - What number represents the first index of an array?
-
+0
 - What is the `length` property of an array?
-
+how many items are in the array
 - How do you calculate the last index of an array?
-
+array.length - 1, -1 because array count starts at 0
 
 ## Notes
 
---
+++ javascript-arrays/main.js
@@ -0,0 +1,27 @@
+const colors = [
+  'red',
+  'white',
+  'blue'
+];
+console.log('value of colors[0]:', colors[0]);
+console.log('value of colors[1]:', colors[1]);
+console.log('value of colors[2]:', colors[2]);
+const america = 'America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2];
+console.log(america);
+colors[2] = 'green';
+const mexico = 'Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2];
+console.log(mexico);
+console.log('Values in colors array:', colors);
+
+const students = [
+  'Nimrod',
+  'Sarah',
+  'Bryan',
+  'Dylan'
+];
+const numberOfStudents = students.length;
+console.log('Value of numberOfStudents:', numberOfStudents);
+const lastIndex = students.length - 1;
+const lastStudent = students[lastIndex];
+console.log('The last student in the array is ', lastStudent);
+console.log('value of students:', students);
