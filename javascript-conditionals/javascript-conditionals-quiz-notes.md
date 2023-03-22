66856e6b0a64e81512c85d4c94cd8cb987cff532
Dylan Dao <dylanpdao@gmail.com>
2023-03-22 21:18:23 +0000

    finished

--- javascript-conditionals/javascript-conditionals-quiz-notes.md
+++ javascript-conditionals/javascript-conditionals-quiz-notes.md
@@ -5,23 +5,28 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - Give 6 examples of comparison operators.
-
+== , !=, ===, >, <, <=, >=, !==
 - What data type do comparison expressions evaluate to?
-
+boolean
 - What is the purpose of an `if` statement?
-
+to decide, if something is true run code block
 - Is `else` required in order to use an `if` statement?
-
+no
 - Describe the syntax (structure) of an `if` statement.
-
+if (condition) {
+  //code//
+}
 - What are the three logical operators?
-
+and &&
+or ||
+not !=
 - How do you compare two different expressions in the same condition?
-
+with the logical operator in the middle
+if (condition && condition)
 - What is the purpose of a `switch` statement?
-
+checks each case, ends when comeplete
 - Is a `default` clause required in order to use a `switch` statement?
-
+yes
 ## Notes
 
 All student notes should be written here.
--- javascript-conditionals/main.js
+++ javascript-conditionals/main.js
@@ -8,3 +8,87 @@
             introduceWarnerBro,
             recommendMovie
  */
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if ((number % 2) === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string) {
+  return (string[0] === 'J');
+}
+
+function isOldEnoughToDrink(person) {
+  return (person.age >= 21);
+}
+
+function isOldEnoughToDrive(person) {
+  return (person.age >= 16);
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+
+function categorizeAcidity(pH) {
+  if (pH < 7 && pH >= 0) {
+    console.log('acid');
+  } else if (pH === 7) {
+    console.log('neutral');
+  } else if (pH > 7 && pH <= 14) {
+    console.log('base');
+  } else {
+    console.log('invalid pH level');
+  }
+}
+
+function introduceWarnerBro(name) {
+  switch (name.toLowerCase()) {
+    case 'yakko':
+      console.log('We\'re the warner brothers!');
+      break;
+    case 'wakko':
+      console.log('We\'re the warner brothers!');
+      break;
+    case 'dot':
+      console.log('I\'m cute~');
+      break;
+    default:
+      console.log('Goodnight everybody!');
+  }
+}
+
+function recommendMovie(genre) {
+  switch (genre.toLowerCase()) {
+    case 'action':
+      console.log('Die Hard');
+      break;
+    case 'comedy':
+      console.log('The Big Lebowski');
+      break;
+    case 'horror':
+      console.log('The Ring');
+      break;
+    case 'drama':
+      console.log('Awaken');
+      break;
+    case 'musical':
+      console.log('High School Musical');
+      break;
+    case 'sci-fi':
+      console.log('The Mandalorian');
+      break;
+    default:
+      console.log('try again bud');
+  }
+}
