677fafa8099207378af5dddaf5b3ea55c3aa96a0
Dylan Dao <dylanpdao@gmail.com>
2023-03-23 21:55:00 +0000

    finish

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,11 @@
 /* exported compact */
+function compact(array) {
+  const arr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Boolean(array[i]) === true) {
+      arr.push(array[i]);
+    }
+  }
+  console.log(arr);
+  return arr;
+}
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,1 +1,8 @@
 /* exported tail */
+function tail(array) {
+  const arr = [];
+  for (let i = 1; i < array.length; i++) {
+    arr.push(array[i]);
+  }
+  return arr;
+}
