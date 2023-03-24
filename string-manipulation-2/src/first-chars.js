59849e394c0a3c0b5cfe9f43cba49da8161fb77c
Dylan Dao <dylanpdao@gmail.com>
2023-03-24 23:07:18 +0000

    finish

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,17 +1,26 @@
 /* exported firstChars */
+// function firstChars(length, string) {
+//   let newStr = '';
+//   if (string === '') {
+//     return '';
+//   } else if (length > string.length) {
+//     for (let i = 0; i < string.length; i++) {
+//       newStr += string[i];
+//     }
+//   } else {
+//     for (let i = 0; i < length; i++) {
+//       newStr += string[i];
+//     }
+//     return newStr;
+//   }
+// }
+
 function firstChars(length, string) {
   let newStr = '';
-  if (string === '') {
-    return '';
-  } else if (length > string.length) {
-    for (let i = 0; i < string.length + 1; i++) {
-      console.log(newStr);
-      newStr += string[i];
-    }
+  if (length > string.length) {
+    newStr += string.slice(0);
   } else {
-    for (let i = 0; i < length; i++) {
-      newStr += string[i];
-    }
-    return newStr;
+    newStr += string.slice(0, length);
   }
+  return newStr;
 }
--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,10 @@
 /* exported lastChars */
+function lastChars(length, string) {
+  let newStr = '';
+  if (length > string.length) {
+    newStr += string.slice(0);
+  } else {
+    newStr += string.slice(string.length - length);
+  }
+  return newStr;
+}
