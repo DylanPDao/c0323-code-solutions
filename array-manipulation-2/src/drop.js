d2ab42e66a6af3009f269483bed6376c69bede5d
Dylan Dao <dylanpdao@gmail.com>
2023-03-24 18:38:11 +0000

    finally

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,1 +1,10 @@
 /* exported drop */
+function drop(array, count) {
+  const arr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i > count - 1) {
+      arr.push(array[i]);
+    }
+  }
+  return arr;
+}
--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,1 +1,10 @@
 /* exported take */
+function take(array, count) {
+  const arr = [];
+  if (array.length !== 0) {
+    for (let i = 0; i < count; i++) {
+      arr.push(array[i]);
+    }
+  }
+  return arr;
+}
