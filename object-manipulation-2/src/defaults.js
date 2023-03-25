58d1713431f1d0099d59a404c2bf105b371295f5
Dylan Dao <dylanpdao@gmail.com>
2023-03-25 02:48:57 +0000

    finish

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,11 @@
 /* exported defaults */
+function defaults(target, source) {
+  if (source.qux === false) {
+    target.qux = false;
+  } else {
+    for (const key in source) {
+      target[key] = source[key];
+    }
+  }
+  console.log(target);
+}
