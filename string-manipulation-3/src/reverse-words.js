ba5e5e8cc067e88f35e33f54cd6857ef652b78ec
Dylan Dao <dylanpdao@gmail.com>
2023-03-27 16:56:17 +0000

    not done

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,8 +1,18 @@
 /* exported reverseWords */
 function reverseWords(string) {
   const newArr = [];
-  // const newString2 = '';
-  for (let i = string.length - 1; i > 0; i--) {
+  const newArr2 = [];
+  let temp = [];
+  for (let i = string.length - 1; i > -1; i--) {
     newArr.push(string[i]);
   }
+  for (let i = 0; i < newArr.length; i++) {
+    temp.push(newArr[i]);
+    if (i === ' ') {
+      newArr2.push(temp);
+      temp = [];
+    }
+  }
+
+  console.log(newArr2);
 }
