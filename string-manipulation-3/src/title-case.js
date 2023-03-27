cc5288e3b4fac8fb3dbe47ced42d9c92d91c2174
Dylan Dao <dylanpdao@gmail.com>
2023-03-27 22:32:51 +0000

    starting over

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,10 +1,21 @@
 /* exported titleCase */
 function titleCase(title) {
-  const newStr = title.replace(/\w\S*/g, word =>
+  let newStr = title.replace(/\w\S*/g, word =>
     word[0].toUpperCase() + word.substring(1).toLowerCase());
 
+  let i;
+  let j;
   const lower = ['And', 'Or', 'Nor', 'But', 'A', 'An', 'The', 'As',
     'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
-  console.log(lower);
-  console.log(newStr);
+  for (i = 0, j = lower.length; i < j; i++) {
+    newStr = newStr.replace(new RegExp('\\s' + lower[i] + '\\s', 'g'),
+      word => word.toLowerCase());
+  }
+
+  const special = ['API', 'JavaScript'];
+  for (i = 0, j = special.length; i < j; i++) {
+    newStr = newStr.replace(new RegExp('\\b' + special[i] + '\\b', 'g'),
+      special[i]);
+  }
+  return newStr;
 }
