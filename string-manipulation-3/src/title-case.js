901821618c67b835fa07547a92ff397766e5a957
Dylan Dao <dylanpdao@gmail.com>
2023-03-27 23:52:21 +0000

    trying

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,21 +1,41 @@
 /* exported titleCase */
-function titleCase(title) {
-  let newStr = title.replace(/\w\S*/g, word =>
-    word[0].toUpperCase() + word.substring(1).toLowerCase());
+// function titleCase(title) {
+//   let newStr = title.replace(/\w\S*/g, word =>
+//     word[0].toUpperCase() + word.substring(1).toLowerCase());
 
+//   let i;
+//   let j;
+//   const lower = ['And', 'Or', 'Nor', 'But', 'A', 'An', 'The', 'As',
+//     'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
+//   for (i = 0, j = lower.length; i < j; i++) {
+//     newStr = newStr.replace(new RegExp('\\s' + lower[i] + '\\s', 'g'),
+//       word => word.toLowerCase());
+//   }
+
+//   const special = ['API', 'JavaScript'];
+//   for (i = 0, j = special.length; i < j; i++) {
+//     newStr = newStr.replace(new RegExp('\\b' + special[i] + '\\b', 'g'),
+//       special[i]);
+//   }
+//   return newStr;
+// }
+
+function titleCase(title) {
   let i;
   let j;
+  let newStr = title.toLowerCase().split(' ').map(word =>
+    word.replace(word[0], word[0].toUpperCase())).join(' ');
+  newStr = newStr.split(' ');
+
   const lower = ['And', 'Or', 'Nor', 'But', 'A', 'An', 'The', 'As',
     'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
-  for (i = 0, j = lower.length; i < j; i++) {
-    newStr = newStr.replace(new RegExp('\\s' + lower[i] + '\\s', 'g'),
-      word => word.toLowerCase());
-  }
-
-  const special = ['API', 'JavaScript'];
-  for (i = 0, j = special.length; i < j; i++) {
-    newStr = newStr.replace(new RegExp('\\b' + special[i] + '\\b', 'g'),
-      special[i]);
+  // debugger;
+  for (i = 0; i < newStr.length; i++) {
+    for (j = 0; j < lower.length; i++) {
+      if (newStr[i] === lower[j]) {
+        newStr[i] = newStr[i].toLowerCase;
+      }
+    }
   }
   return newStr;
 }
