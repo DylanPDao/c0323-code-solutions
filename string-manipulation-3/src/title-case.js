c6e89f9c6f5dbe9f87f7c92a8786ad27aaaf5fea
Dylan Dao <dylanpdao@gmail.com>
2023-03-27 21:28:00 +0000

    adding lower parameters

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,6 +1,10 @@
 /* exported titleCase */
 function titleCase(title) {
-  const newStr = title.split(' ')
-    .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase());
-  return newStr.join(' ');
+  const newStr = title.replace(/\w\S*/g, word =>
+    word[0].toUpperCase() + word.substring(1).toLowerCase());
+
+  const lower = ['And', 'Or', 'Nor', 'But', 'A', 'An', 'The', 'As',
+    'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
+  console.log(lower);
+  console.log(newStr);
 }
