6cdacb48f5aae6fa099d12e2629158685470c2fd
Dylan Dao <dylanpdao@gmail.com>
2023-03-28 03:59:41 +0000

    working on including : in if statements

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,25 +1,4 @@
 /* exported titleCase */
-// function titleCase(title) {
-//   let newStr = title.replace(/\w\S*/g, word =>
-//     word[0].toUpperCase() + word.substring(1).toLowerCase());
-
-//   let i;
-//   let j;
-//   const lower = ['And', 'Or', 'Nor', 'But', 'A', 'An', 'The', 'As',
-//     'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
-//   for (i = 0, j = lower.length; i < j; i++) {
-//     newStr = newStr.replace(new RegExp('\\s' + lower[i] + '\\s', 'g'),
-//       word => word.toLowerCase());
-//   }
-
-//   const special = ['API', 'JavaScript'];
-//   for (i = 0, j = special.length; i < j; i++) {
-//     newStr = newStr.replace(new RegExp('\\b' + special[i] + '\\b', 'g'),
-//       special[i]);
-//   }
-//   return newStr;
-// }
-
 function titleCase(title) {
   let i;
   let j;
@@ -29,23 +8,22 @@ function titleCase(title) {
   const lower = ['And', 'Or', 'Nor', 'But', 'A', 'An', 'The', 'As',
     'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
   for (i = 0; i < newStr.length; i++) {
-    if (newStr[i] === 'Javascript') {
+    if (newStr[i].includes('Javascript')) {
       newStr[i] = 'JavaScript';
+      // if (newStr[i].includes(':')) {
+      //   console.log(newStr[i]);
+      // }
       continue;
     }
     if (newStr[i] === 'Api') {
-      console.log('hello');
       newStr[i] = 'API';
       continue;
     }
     if (newStr[i].indexOf('-') !== -1) {
       const index = newStr[i].indexOf('-');
-      console.log(index);
-      // newStr[i] = newStr[i][0].toUpperCase() + newStr[i].slice(1, newStr[i].length);
       const newArr = newStr[i].split('');
       newArr[index + 1] = newArr[index + 1].toUpperCase();
       newStr[i] = newArr.join('');
-      console.log(newArr);
     }
     for (j = 0; j < lower.length; j++) {
       if (newStr[i] === lower[j]) {
