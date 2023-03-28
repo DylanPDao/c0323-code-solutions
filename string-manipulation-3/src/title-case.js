a4c888d4692edecb86edde9786c87babc1da7641
Dylan Dao <dylanpdao@gmail.com>
2023-03-28 03:06:40 +0000

    added 2 conditions for JavaScript and API

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -23,19 +23,37 @@
 function titleCase(title) {
   let i;
   let j;
-  let newStr = title.toLowerCase().split(' ').map(word =>
-    word.replace(word[0], word[0].toUpperCase())).join(' ');
-  newStr = newStr.split(' ');
+  const newStr = title.toLowerCase().split(' ').map(word =>
+    word.replace(word[0], word[0].toUpperCase()));
 
   const lower = ['And', 'Or', 'Nor', 'But', 'A', 'An', 'The', 'As',
     'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
-  // debugger;
   for (i = 0; i < newStr.length; i++) {
-    for (j = 0; j < lower.length; i++) {
+    if (newStr[i] === 'Javascript') {
+      newStr[i] = 'JavaScript';
+      continue;
+    }
+    if (newStr[i] === 'Api') {
+      console.log('hello');
+      newStr[i] = 'API';
+      continue;
+    }
+    if (newStr[i].indexOf('-') !== -1) {
+      const index = newStr[i].indexOf('-');
+      console.log(index);
+      // newStr[i] = newStr[i][0].toUpperCase() + newStr[i].slice(1, newStr[i].length);
+      const newArr = newStr[i].split('');
+      newArr[index + 1] = newArr[index + 1].toUpperCase();
+      newStr[i] = newArr.join('');
+      console.log(newArr);
+    }
+    for (j = 0; j < lower.length; j++) {
       if (newStr[i] === lower[j]) {
-        newStr[i] = newStr[i].toLowerCase;
+        newStr[i] = newStr[i].toLowerCase();
       }
     }
   }
-  return newStr;
+  const final = newStr.join(' ');
+  const final1 = final[0].toUpperCase() + final.slice(1, final.length);
+  return final1;
 }
