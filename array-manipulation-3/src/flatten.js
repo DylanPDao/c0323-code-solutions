d391849db56e8e592a9728c782856e3536edd577
Dylan Dao <dylanpdao@gmail.com>
2023-03-28 20:58:19 +0000

    finish flatten

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,5 @@
 /* exported flatten */
+function flatten(array) {
+  const flat = [].concat.apply([], array);
+  return flat;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,6 @@
 /* exported zip */
+// function zip(first, second) {
+//   const third = first.map(e, i => {
+//     return [e, second[i]];
+//   });
+// }
