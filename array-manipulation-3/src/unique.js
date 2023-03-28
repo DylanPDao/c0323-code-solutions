2b6f9db42a63d3478163ef962448275fd7719494
Dylan Dao <dylanpdao@gmail.com>
2023-03-28 23:34:40 +0000

    making loop

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,20 @@
 /* exported unique */
+function unique(array) {
+  const newArr = [];
+  let i;
+  let j;
+  // return empty array if empty array is input
+  if (array.length === 0) {
+    return array;
+  } else {
+    for (i = 0; i < array.length; i++) {
+      for (j = newArr.length + 1; j > 0; j--) {
+        if (array[i] !== newArr[j]) {
+          newArr.push(array[i]);
+        }
+      }
+    }
+    console.log(newArr);
+    return newArr;
+  }
+}
