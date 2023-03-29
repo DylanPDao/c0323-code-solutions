babdeb7fe97619cdb9f4f345e67a7ab7d120fd22
Dylan Dao <dylanpdao@gmail.com>
2023-03-29 16:01:40 +0000

    created deck and players

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,1 +1,23 @@
-console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// function runGame(array) {
+//   const players = {
+//     Shawn: 2,
+//     Brian: 2,
+//     Sarah: 2,
+//     Dylan: 2
+//   };
+
+// }
+
+// function getDeck() {
+//   const suits = ['spades', 'diamonds', 'clubs', 'hearts'];
+//   const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+//   const deck = [];
+
+//   for (let i = 0; i < suits.length; i++) {
+//     for(let j = 0; j < values.length; j++) {
+//       let card =
+//     }
+//   }
+// }
