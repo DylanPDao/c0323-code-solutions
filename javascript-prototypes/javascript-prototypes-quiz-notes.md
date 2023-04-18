397571ede5be50b87b79c3a0a4ecea024d8d5cd1
Dylan Dao <dylanpdao@gmail.com>
2023-04-18 01:05:15 +0000

    finished

--- javascript-prototypes/javascript-prototypes-quiz-notes.md
+++ javascript-prototypes/javascript-prototypes-quiz-notes.md
@@ -5,13 +5,13 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - What kind of inheritance does the JavaScript programming language use?
-
+protype-based or protoypal
 - What is a prototype in JavaScript?
-
+on object that holds methods we would like to reuse
 - How is it possible to call methods on strings, arrays, and numbers even though those methods don't actually exist on strings, arrays, and numbers?
-
+borrowa the methods when needed
 - If an object does not have it's own property or method by a given key, where does JavaScript look for it?
-
+_proto_
 ## Notes
 
 All student notes should be written here.
--- javascript-prototypes/warner-bros.js
+++ javascript-prototypes/warner-bros.js
@@ -3,32 +3,28 @@
 const yakko = {
   age: 14,
   name: 'Yakko',
-  role: 'brother',
-  describe: function () {
-    const description = this.name + ' is a ' +
-                      this.age + '-year-old Warner ' + this.role + '.';
-    return description;
-  }
+  role: 'brother'
 };
 
 const wakko = {
   age: 11,
   name: 'Wakko',
-  role: 'brother',
-  describe: function () {
-    const description = this.name + ' is a ' +
-                      this.age + '-year-old Warner ' + this.role + '.';
-    return description;
-  }
+  role: 'brother'
 };
 
 const dot = {
   age: 10,
   name: 'Dot',
-  role: 'sister',
+  role: 'sister'
+};
+
+const warnerPrototype = {
   describe: function () {
-    const description = this.name + ' is a ' +
-                      this.age + '-year-old Warner ' + this.role + '.';
-    return description;
+    return this.name + ' is a ' +
+      this.age + '-year-old Warner ' + this.role + '.';
   }
 };
+
+Object.setPrototypeOf(yakko, warnerPrototype);
+Object.setPrototypeOf(wakko, warnerPrototype);
+Object.setPrototypeOf(dot, warnerPrototype);
