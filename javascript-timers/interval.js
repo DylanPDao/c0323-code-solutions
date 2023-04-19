4bb37064867907cec5a2c8b37681eafb8839a0ef
Dylan Dao <dylanpdao@gmail.com>
2023-04-19 17:24:47 +0000

    took second function outside to setInterval.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -3,10 +3,13 @@ const head = document.querySelector('.countdown-display');
 const myInt = setInterval(() => {
   if (head.textContent === '1') {
     clearInterval(myInt);
-    setTimeout(() => {
-      head.textContent = '~ Earth Beeeelooowww Us~';
-    }, 500);
   } else {
     head.textContent = head.textContent -= 1;
   }
 }, 1000);
+
+setInterval(() => {
+  if (head.textContent === '1') {
+    head.textContent = '~ Earth Beeeelooowww Us~';
+  }
+}, 2000);
