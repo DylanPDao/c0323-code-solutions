364604a65301a085062f4ff5e525557568dad8b5
Dylan Dao <dylanpdao@gmail.com>
2023-04-19 17:33:05 +0000

    took out extra setInterval

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -3,13 +3,8 @@ const head = document.querySelector('.countdown-display');
 const myInt = setInterval(() => {
   if (head.textContent === '1') {
     clearInterval(myInt);
+    head.textContent = '~ Earth Beeeelooowww Us~';
   } else {
     head.textContent = head.textContent -= 1;
   }
 }, 1000);
-
-setInterval(() => {
-  if (head.textContent === '1') {
-    head.textContent = '~ Earth Beeeelooowww Us~';
-  }
-}, 2000);
