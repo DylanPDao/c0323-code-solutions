c7da5f7c4783b1fd682772a32015e8f52e3a6b68
Dylan Dao <dylanpdao@gmail.com>
2023-05-09 17:59:29 +0000

    changed index to array length - 1

--- array-methods/for-each.js
+++ array-methods/for-each.js
@@ -2,6 +2,5 @@ const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
 console.log('In order:');
 values.forEach((e) => console.log(e));
 
-const valuesReversed = values.reverse();
 console.log('Reverse order:');
-values.forEach((e, index) => console.log(values[index]));
+values.forEach((e, index) => console.log(values[values.length - index - 1]));
