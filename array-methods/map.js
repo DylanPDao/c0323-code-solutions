22729ecfd41c57bcc6f0851632053df36a2a9d09
Dylan Dao <dylanpdao@gmail.com>
2023-05-09 17:42:22 +0000

    added dollar sign

--- array-methods/map.js
+++ array-methods/map.js
@@ -2,5 +2,5 @@ const prices = [42.42, 10, 28.2234, 3.2, 5, 12];
 const priceObject = prices.map((e) => ({ price: e, salePrice: e / 2 }));
 console.log('Priced object', priceObject);
 
-const formattedPrices = prices.map((e) => `${e.toFixed(2)}`);
+const formattedPrices = prices.map((e) => `$${e.toFixed(2)}`);
 console.log('Formatted prices', formattedPrices);
