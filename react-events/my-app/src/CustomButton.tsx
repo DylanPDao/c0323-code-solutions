8324f27570cc5b10c0a3237581788513471d6f6c
Dylan Dao <dylanpdao@gmail.com>
2023-05-19 21:11:42 +0000

    took out some files

--- react-events/my-app/src/CustomButton.tsx
+++ react-events/my-app/src/CustomButton.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-
 type Props = {
   text: string;
   color: string;
   onCustomClick: Function;
 };
 
-export default function CustomButton({ text, color, onCustomClick }: Props) {
+export default function CustomButton({
+  text,
+  color,
+  onCustomClick,
+}: Props): JSX.Element {
   return (
     <button
       onClick={() => onCustomClick(color, text)}
