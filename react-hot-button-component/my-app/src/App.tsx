b22371ad9ac646cf0e80679df77f07060f1cd3aa
Dylan Dao <dylanpdao@gmail.com>
2023-05-21 23:28:24 +0000

    starting files

---
+++ react-hot-button-component/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CustomButton from './CustomButton';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CustomButton text="Hot Button" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-hot-button-component/my-app/src/CustomButton.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+type Props = {
+  text: string;
+};
+
+export default function CustomButton({ text }: Props) {
+  let [index, setClicked] = useState(0);
+
+  function handleClick(): void {
+    setClicked(index + 1);
+  }
+
+  let className = 'btn';
+  if (index < 3) {
+    className = 'btn';
+  } else if (index < 6) {
+    className = 'btn light-purple';
+  } else if (index < 9) {
+    className = 'btn coral';
+  } else if (index < 12) {
+    className = 'btn salmon';
+  } else if (index < 15) {
+    className = 'btn yellow';
+  } else {
+    className = 'btn white';
+  }
+
+  return (
+    <div>
+      <button className={className} onClick={handleClick}>
+        {text}
+      </button>
+    </div>
+  );
+}
