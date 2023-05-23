d9c2d300057a2b88296d320a5dd5f66726dc9af2
Dylan Dao <dylanpdao@gmail.com>
2023-05-23 17:59:03 +0000

    edited

--- react-validated-input-component/my-app/src/ValidateInput.tsx
+++ react-validated-input-component/my-app/src/ValidateInput.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
 
 export default function ValidateInput() {
   const [inputValue, setInputValue] = useState('');
@@ -7,28 +8,15 @@ export default function ValidateInput() {
     '(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'
   );
 
-  let inputValueLength: number = inputValue.length;
-  let xIHidden: string = 'hidden';
-  let checkHidden: string = 'hidden';
-  let shortPassword: string = 'hidden';
-  let noPassword: string = 'hidden';
-  let regexPass: string = 'hidden';
-
-  if (inputValueLength === 0 || inputValueLength < 8) {
-    xIHidden = '';
-  }
-  if (inputValueLength === 0) {
-    noPassword = '';
-  }
-  if (inputValueLength >= 8 && regex.test(inputValue)) {
-    checkHidden = '';
-  }
-  if (inputValueLength < 8 && inputValueLength > 0) {
-    shortPassword = '';
-  }
-  if (inputValueLength > 8 && !regex.test(inputValue)) {
-    regexPass = '';
-  }
+  let inputValueLength = inputValue.length;
+  const xIHidden = inputValueLength === 0 || inputValueLength < 8 || 'hidden';
+  const checkHidden =
+    (inputValueLength >= 8 && regex.test(inputValue)) || 'hidden';
+  const shortPassword =
+    (inputValueLength < 8 && inputValueLength > 0) || 'hidden';
+  const noPassword = inputValueLength === 0 || 'hidden';
+  const regexPass =
+    (inputValueLength > 8 && !regex.test(inputValue)) || 'hidden';
 
   return (
     <form
@@ -47,14 +35,16 @@ export default function ValidateInput() {
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <i className={`bi bi-x ${xIHidden}`}></i>
-        <i className={`bi bi-check-lg ${checkHidden}`}></i>
+        <i className={classNames('bi bi-x', xIHidden)}></i>
+        <i className={classNames('bi bi-check-lg', checkHidden)}></i>
       </div>
-      <p className={`no-pass ${noPassword}`}>A password is required.</p>
-      <p className={`short-pass ${shortPassword}`}>
+      <p className={classNames('no-pass', noPassword)}>
+        A password is required.
+      </p>
+      <p className={classNames('short-pass', shortPassword)}>
         Your password is too short
       </p>
-      <p className={`short-pass ${regexPass}`}>
+      <p className={classNames('short-pass', regexPass)}>
         Password must contain 1 uppercase, 1 lowercase, 1 special character, and
         1 number
       </p>
