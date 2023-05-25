226327d61d3d8dd86486f81db2596cc14bbf2d26
Dylan Dao <dylanpdao@gmail.com>
2023-05-25 01:48:39 +0000

    added headers

--- react-app-drawer-component/my-app/src/Menu.tsx
+++ react-app-drawer-component/my-app/src/Menu.tsx
@@ -3,18 +3,23 @@ import { useState } from 'react';
 
 export default function Menu({ content }: { content: string[] }): JSX.Element {
   const [isMenuOpen, setMenuOpen] = useState(true);
+  const [isActiveIndex, setActiveIndex] = useState(content.length + 1);
 
   return (
     <div>
-      <GiHamburgerMenu
-        className="hamburger-menu-icon"
-        onClick={() => setMenuOpen(!isMenuOpen)}
-      />
-      <MenuShade isClose={() => setMenuOpen(!isMenuOpen)} isOpen={isMenuOpen} />
+      <div className="header">
+        <GiHamburgerMenu
+          className="hamburger-menu-icon"
+          onClick={() => setMenuOpen(!isMenuOpen)}
+        />
+        <HeaderComp activeIndex={isActiveIndex} content={content} />
+      </div>
+      <MenuShade onClose={() => setMenuOpen(!isMenuOpen)} isOpen={isMenuOpen} />
       <MenuContent
         isOpen={isMenuOpen}
         content={content}
-        isClose={() => setMenuOpen(!isMenuOpen)}
+        onClose={() => setMenuOpen(!isMenuOpen)}
+        activeIndex={(i: number) => setActiveIndex(i)}
       />
     </div>
   );
@@ -23,14 +28,26 @@ export default function Menu({ content }: { content: string[] }): JSX.Element {
 type MenuContentTypes = {
   content: string[];
   isOpen: boolean;
-  isClose: () => void;
+  onClose: () => void;
+  activeIndex: (i: number) => void;
 };
 
-function MenuContent({ content, isOpen, isClose }: MenuContentTypes) {
+function MenuContent({
+  content,
+  isOpen,
+  onClose,
+  activeIndex,
+}: MenuContentTypes) {
   const con = [];
   for (let i = 0; i < content.length; i++) {
     con.push(
-      <p onClick={() => isClose()} className={`menu-item num${i}`} key={i}>
+      <p
+        onClick={() => {
+          onClose();
+          activeIndex(i);
+        }}
+        className={`menu-item num${i}`}
+        key={i}>
         {content[i]}
       </p>
     );
@@ -42,15 +59,25 @@ function MenuContent({ content, isOpen, isClose }: MenuContentTypes) {
 }
 
 function MenuShade({
-  isClose,
+  onClose,
   isOpen,
 }: {
-  isClose: () => void;
+  onClose: () => void;
   isOpen: boolean;
 }): JSX.Element {
   return (
     <div
       className={isOpen ? 'menu-shade' : 'menu-shade is-drawn'}
-      onClick={() => isClose()}></div>
+      onClick={onClose}></div>
   );
+}
+
+function HeaderComp({
+  activeIndex,
+  content,
+}: {
+  activeIndex: number | undefined;
+  content: string[];
+}): JSX.Element {
+  return activeIndex ? <h1>{content[activeIndex]}</h1> : <h1>HOME</h1>;
 }
