64dd8d35463d21b5c5d75c04748387ee520ed27f
Dylan Dao <dylanpdao@gmail.com>
2023-05-26 21:56:33 +0000

    added errors if page not loaded

--- fetch-in-react/my-app/src/User.js
+++ fetch-in-react/my-app/src/User.js
@@ -13,6 +13,9 @@ export default function User({ userId, onCancel }) {
     async function getData() {
       try {
         const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
         const data = await response.json();
         setUser(data);
       } catch (err) {
--- fetch-in-react/my-app/src/Users.js
+++ fetch-in-react/my-app/src/Users.js
@@ -16,6 +16,9 @@ export default function Users() {
         const response = await fetch(
           'https://jsonplaceholder.typicode.com/users'
         );
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
         const data = await response.json();
         setUsers(data);
       } catch (err) {
