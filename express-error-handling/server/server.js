a69540c63a5e76f6911b0d0fe688033bd54b03ff
Dylan Dao <dylanpdao@gmail.com>
2023-05-30 16:56:34 +0000

    finished

--- express-error-handling/server/server.js
+++ express-error-handling/server/server.js
@@ -15,7 +15,7 @@ const db = new pg.Pool({
 const app = express();
 app.use(express.json());
 
-app.get('/api/grades', async (req, res) => {
+app.get('/api/grades', async (req, res, next) => {
   try {
     const sql = `
       select *
@@ -25,26 +25,19 @@ app.get('/api/grades', async (req, res) => {
     const grades = result.rows;
     res.json(grades);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'an unexpected error occurred' });
+    next(err);
   }
 });
 
-app.post('/api/grades', async (req, res) => {
+app.post('/api/grades', async (req, res, next) => {
   try {
     const { name, course } = req.body;
     const score = Number(req.body.score);
     if (!Number.isInteger(score) || score < 0 || score > 100) {
-      res
-        .status(400)
-        .json({ error: 'score must be an integer between 0 and 100' });
-      return;
+      throw new ClientError(400, `score must be an integer between 0 and 100`);
     }
     if (!name || !course) {
-      res
-        .status(400)
-        .json({ error: 'name, course, and score are required fields' });
-      return;
+      throw new ClientError(400, `name, course, and score are required fields`);
     }
     const sql = `
       insert into "grades" ("name", "course", "score")
@@ -57,18 +50,15 @@ app.post('/api/grades', async (req, res) => {
     res.status(201).json(newGrade);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'an unexpected error occurred' });
+    next(err);
   }
 });
 
-app.get('/api/grades/:gradeId', async (req, res) => {
+app.get('/api/grades/:gradeId', async (req, res, next) => {
   try {
     const gradeId = Number(req.params.gradeId);
     if (!Number.isInteger(gradeId) || gradeId < 1) {
-      res.status(400).json({
-        error: 'grade must be a positive integer',
-      });
-      return;
+      throw new ClientError(400, `gradeId must be a positive integer`);
     }
     const sql = `
       select *
@@ -79,38 +69,28 @@ app.get('/api/grades/:gradeId', async (req, res) => {
     const result = await db.query(sql, params);
     const [grade] = result.rows;
     if (!grade) {
-      res
-        .status(404)
-        .json({ error: `cannot find grade with gradeId ${gradeId}` });
+      throw new ClientError(400, `Cannot find that grade id ${gradeId}`);
     } else {
       res.json(grade);
     }
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'an unexpected error occurred' });
+    next(err);
   }
 });
 
-app.put('/api/grades/:gradeId', async (req, res) => {
+app.put('/api/grades/:gradeId', async (req, res, next) => {
   try {
     const gradeId = Number(req.params.gradeId);
     if (!Number.isInteger(gradeId) || gradeId < 1) {
-      res.status(400).json({ error: 'grade must be a positive integer' });
-      return;
+      throw new ClientError(400, `gradeId must be a positive integer`);
     }
     const { name, course } = req.body;
     const score = Number(req.body.score);
     if (!Number.isInteger(score) || score < 0 || score > 100) {
-      res
-        .status(400)
-        .json({ error: 'score must be an integer between 0 and 100' });
-      return;
+      throw new ClientError(400, `score must be an integer between 0 and 100`);
     }
     if (!name || !course) {
-      res
-        .status(400)
-        .json({ error: 'name, course, and score are required fields' });
-      return;
+      throw new ClientError(400, `name, course, and score are required fields`);
     }
     const sql = `
       update "grades"
@@ -124,24 +104,20 @@ app.put('/api/grades/:gradeId', async (req, res) => {
     const result = await db.query(sql, params);
     const [updatedGrade] = result.rows;
     if (!updatedGrade) {
-      res
-        .status(404)
-        .json({ error: `cannot find grade with gradeId ${gradeId}` });
+      throw new ClientError(400, `Cannot find that grade id ${gradeId}`);
     } else {
       res.json(updatedGrade);
     }
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'an unexpected error occurred' });
+    next(err);
   }
 });
 
-app.delete('/api/grades/:gradeId', async (req, res) => {
+app.delete('/api/grades/:gradeId', async (req, res, next) => {
   try {
     const gradeId = Number(req.params.gradeId);
     if (!Number.isInteger(gradeId) || gradeId < 1) {
-      res.status(400).json({ error: 'grade must be a positive integer' });
-      return;
+      throw new ClientError(400, `gradeId must be a positive integer`);
     }
     const sql = `
       delete from "grades"
@@ -152,15 +128,12 @@ app.delete('/api/grades/:gradeId', async (req, res) => {
     const result = await db.query(sql, params);
     const [deletedGrade] = result.rows;
     if (!deletedGrade) {
-      res
-        .status(404)
-        .json({ error: `cannot find grade with gradeId ${gradeId}` });
+      throw new ClientError(400, `gradeId does not exist`);
     } else {
       res.sendStatus(204);
     }
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'an unexpected error occurred' });
+    next(err);
   }
 });
 
