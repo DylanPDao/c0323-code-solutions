9fc23020ab33d7596955d69635194f3ab38c1793
Dylan Dao <dylanpdao@gmail.com>
2023-05-31 16:53:28 +0000

    moved browser router

--- react-routing/client/src/App.js
+++ react-routing/client/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
 import Catalog from './pages/Catalog';
 import ProductDetails from './pages/ProductDetails';
 import About from './pages/About';
@@ -9,18 +8,16 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Header />}>
-            <Route index element={<Catalog />} />
-            <Route path="details/:productId" element={<ProductDetails />} />
-            <Route path="about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Catalog />} />
+          <Route path="details/:productId" element={<ProductDetails />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
