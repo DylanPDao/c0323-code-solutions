0dbc14016b4038018e27c47b9a813bdde6e675c0
Dylan Dao <dylanpdao@gmail.com>
2023-06-01 17:23:57 +0000

    destructured for cleaner code

--- react-custom-hooks/client/src/App.js
+++ react-custom-hooks/client/src/App.js
@@ -6,10 +6,7 @@ import TodoForm from './components/TodoForm';
 import { useTodos } from './components/useTodos';
 
 export default function App() {
-  // TODO: Use the custom hook here instead of state and functions
-  const [error, setError] = useState();
-
-  const useTodo = useTodos();
+  const { addTodo, isLoading, todos, toggleCompleted, error } = useTodos();
 
   if (error) {
     console.error('Fetch error:', error);
@@ -21,13 +18,10 @@ export default function App() {
       <div className="row">
         <div className="col pt-5">
           <PageTitle text="Todo App" />
-          <TodoForm onSubmit={useTodo.addTodo} />
-          {useTodo.isLoading && <div>Loading...</div>}
-          {!useTodo.isLoading && (
-            <TodoList
-              todos={useTodo.todos}
-              toggleCompleted={useTodo.toggleCompleted}
-            />
+          <TodoForm onSubmit={addTodo} />
+          {isLoading && <div>Loading...</div>}
+          {!isLoading && (
+            <TodoList todos={todos} toggleCompleted={toggleCompleted} />
           )}
         </div>
       </div>
